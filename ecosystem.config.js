a996dfefd29e853e26436aa91d2208a9233c99de
Gadya <zakuson2001@inbox.ru>
2023-09-27 00:36:14 +0300

    add pm2 ecosystem file to project

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+        name: "alavdeev_bot",
+        cwd: "/home/botmanager/alavdeev_bot/",
+        script: "./alavdeev_bot.py",
+        watch: true,
+        interpreter: "./venv/bin/python3"
+    },
+    ]
+};
