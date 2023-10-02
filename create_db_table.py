36b0482ed8c2161208f2440fd5855bf724f50d3f
Gadya <zakuson2001@inbox.ru>
2023-10-02 22:56:15 +0300

    modify create_db_table.py

--- create_db_table.py
+++ create_db_table.py
@@ -2,11 +2,15 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from alavdeev_bot_utils.constants import engine
 from alavdeev_bot_utils.bot_models import Base
+import sqlalchemy
 
 
 engine_inst = create_engine(engine)
 try:
-    Base.metadata.create_all(engine_inst)
-    print("Таблицы созданы успешно")
-except Exception:
-    print("Что-то не так или таблицы уже есть")
+    if not sqlalchemy.inspect(engine).has_table("USERS"):
+        Base.metadata.create_all(engine_inst)
+        print("Таблицы созданы успешно")
+    else:
+        print("Таблицы уже есть")
+except Exception as e:
+    print(f"Что-то не так:\n{e}")
